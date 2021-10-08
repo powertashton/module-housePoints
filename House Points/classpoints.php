9a08b85868b02bffb4bbf4752a94f3d5b472bdd5
Ashton Power <48048360+powertashton@users.noreply.github.com>
2021-10-08 08:54:08 +0800

    Class and Individual Points views - Removing some AJAX/JS (#5)
    * Initial Change
    
    * HousePointsHouse
    
    * Student House Points initial
    
    * More refactoring
    
    * Update manage.php
    
    * Further Refactoring
    
    * Refactoring and Removing non-inline JS/JQuery
    
    * Update HousePointStudentGateway.php
    
    * classpoints
    
    * Class and Individual Points

--- House Points/classpoints.php
+++ House Points/classpoints.php
@@ -18,6 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 use Gibbon\Forms\DatabaseFormFactory;
 use Gibbon\Forms\Form;
+use Gibbon\Services\Format;
+use Gibbon\Module\HousePoints\Domain\HousePointStudentGateway;
+use Gibbon\Domain\User\UserGateway;
+use Gibbon\Tables\DataTable;
 
 require_once __DIR__ . '/moduleFunctions.php';
 
@@ -32,58 +36,49 @@ if (isActionAccessible($guid, $connection2,"/modules/House Points/classpoints.ph
     $form->setFactory(DatabaseFormFactory::create($pdo));
     $form->addHiddenValue('yearID', $session->get('gibbonSchoolYearID'));
     
+    $classID = $_POST['classID'] ?? '' ;
     $row = $form->addRow();
         $row->addLabel('classID', __('Class'));
-        $row->addSelectFormGroup('classID', $session->get('gibbonSchoolYearID'))->placeholder();
+        $row->addSelectFormGroup('classID', $session->get('gibbonSchoolYearID'))->placeholder()->selected($classID);
     
+    $row = $form->addRow();
+        $row->addFooter();
+        $row->addSubmit();
     echo $form->getOutput();
     
-    echo "<br><div id='studentPoints'></div>";
-
-
-    //TODO: refine the JS and AJAX and maybe change the table into OO
-    ?>
-    <script>
-        $('#classID').change(function() {
-            showClassPoint($('#classID').val(), $('input:hidden[name=yearID]').val());
+    
+    if(isset($_POST['classID'])){
+        $housePointStudentGateway = $container->get(HousePointStudentGateway::class);
+        $data = $housePointStudentGateway->selectClassStudentPoints($_POST['classID'], $_POST['yearID'])->fetchAll();
+        
+        $table = DataTable::create('classPoints');
+        $table->setTitle('Class House Points');
+        
+        $userGateway = $container->get(UserGateway::class);
+        $table->addColumn('gibbonPersonID', __('Name'))->format(function ($row) use ($userGateway) {
+            $person = $userGateway->getByID($row['gibbonPersonID']);
+            $output = Format::name($person['title'], $person['preferredName'], $person['surname'], 'Student');
+            return $output;
         });
-        function showClassPoint(classID, yearID) {    
-            $.ajax({
-                url: "./modules/House Points/classpoints_ajax.php",
-                data: {
-                    classID: classID,
-                    yearID: yearID
-                },
-                type: 'POST',
-                dataType: 'JSON',
-                success: function(data) {
-                    console.log(data);
-                    var points = data.points;
-                    var total;
-            
-                    var html = '';
-                    html += "<table style='width:100%;'>";
-                        html += "<tr>";
-                            html += "<th style='width:40%;'>Name</th>";
-                            html += "<th style='width:25%;'>Points</th>";
-                            html += "<th style='width:15%;'>House</th>";
-                        html += "</tr>";
-                        $.each(points, function(i, pt) {
-                            total = pt.total === null ? 0 : pt.total;
-                            html += "<tr>";
-                                html += "<td>" + pt.surname + ", " + pt.preferredName + "</td>";
-                                html += "<td>" + total + "</td>";
-                                html += "<td>" + pt.houseName + "</td>";
-                            html += "</tr>";
-                        });
-                    html += "</table>";
-            
-                    $('#studentPoints').html(html);
-                }
-            });
-        }
-    </script>
-    <?php
+        $table->addColumn('total', __('Total Points'))->format(function ($row) {
+            if ($row['total'] == ''){
+                $output='0';
+            } else {
+                $output=$row['total'];
+            }
+            return $output;
+        });
+        $table->addColumn('houseName', __('House'))->format(function ($row) {
+            if ($row['houseName'] == ''){
+                $output='N/A';
+            } else {
+                $output=$row['houseName'];
+            }
+            return $output;
+        });
+    
+        echo $table->render($data);
+    }
 
 
 }
--- House Points/individual.php
+++ House Points/individual.php
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 use Gibbon\Forms\DatabaseFormFactory;
 use Gibbon\Forms\Form;
+use Gibbon\Module\HousePoints\Domain\HousePointStudentGateway;
+use Gibbon\Tables\DataTable;
 
 require_once __DIR__ . '/moduleFunctions.php';
 
@@ -31,67 +33,35 @@ if (isActionAccessible($guid, $connection2,"/modules/House Points/individual.php
     $form->setFactory(DatabaseFormFactory::create($pdo));
     $form->addHiddenValue('yearID', $session->get('gibbonSchoolYearID'));
     
+    $studentID = $_POST['studentID'] ?? '';
     $row = $form->addRow();
         $row->addLabel('studentID', __('Student'));
-        $row->addSelectStudent('studentID', $session->get("gibbonSchoolYearID"), array())->placeholder();
+        $row->addSelectStudent('studentID', $session->get("gibbonSchoolYearID"), array())->placeholder()->selected($studentID);
 
+    $row = $form->addRow();
+        $row->addFooter();
+        $row->addSubmit();
     echo $form->getOutput();
     
-    echo "<br><div id='studentPoints'></div>";
+    if(isset($_POST['studentID'])){
+        $housePointStudentGateway = $container->get(HousePointStudentGateway::class);
+        $housePoints = $housePointStudentGateway->selectStudentPoints($_POST['studentID'], $_POST['yearID'])->fetchAll();
+        $housePointsAll = $housePointStudentGateway->selectStudentPointsSum($_POST['studentID'], $_POST['yearID'])->fetch();
+
+        $table = DataTable::create('housePoints');
+        $table->setTitle('House Points');
     
-    //TODO: refine the JS and AJAX and maybe change the table into OO
-    ?>
-        <script>
-        $('#studentID').change(function() {
-            showIndividualPoint($('#studentID').val());
-        });
-        function showIndividualPoint(studentID) {
-            $.ajax({
-                url: "./modules/House Points/individual_ajax.php",
-                data: {
-                    studentID: studentID
-                },
-                type: 'POST',
-                dataType: 'JSON',
-                success: function(data) {
-                    console.log(data);
-                    var points = data.points;
-            
-                    var html = '';
-                    if (points.length === 0) {
-                        html = "<p>No points have been awarded to this student</p>";
-                    } else {
-                        var total = 0;
-                        html += "<table style='width:100%;'>";
-                            html += "<tr>";
-                                html += "<th style='width:15%;'>Date</th>";
-                                html += "<th style='width:10%;'>Points</th>";
-                                html += "<th style='width:15%;'>Category</th>";
-                                html += "<th style='width:30%;'>Reason</th>";
-                                html += "<th style='width:20%;'>Awarded By</th>";
-                            html += "</tr>";
-                            $.each(points, function(i, pt) {
-                                total += parseInt(pt.points);
-                                html += "<tr>";
-                                    html += "<td>" + pt.awardedDate + "</td>";
-                                    html += "<td>" + pt.points + "</td>";
-                                    html += "<td>" + pt.categoryName + "</td>";
-                                    html += "<td>" + pt.reason + "</td>";
-                                    html += "<td>" + pt.teacherName + "</td>";
-                                html += "</tr>";
-                            });
-                            html += "<tr>";
-                                html += "<td><strong>Total</strong></td>";
-                                html += "<td colspan='4'><strong>" + total + " points</strong></td>";
-                            html += "</tr>";
-                        html += "</table>";
-                    }
-            
-                    $('#studentPoints').html(html);
-                }
-            });
-        }
-        </script>
-    <?php
+
+        $table->addColumn('awardedDate', __('Date'));
+        $table->addColumn('points', __('Points'));
+        $table->addColumn('categoryName', __('Category'));
+        $table->addColumn('reason', __('Reason'));
+        $table->addColumn('teacherName', __('Awarded By'));
+    
+        echo $table->render($housePoints);
+    
+        //TODO: It may be worth creating a table object for a footer row in the future...
+        echo "<b>Total: </b>". $housePointsAll['points'];
+    }
 }
 
